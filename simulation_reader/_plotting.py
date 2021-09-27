478a60b4c737e1d7ba4ddf17466f67e0a325b78e
Zachary Hardy <zhardy1115@gmail.com>
2021-09-27 16:15:20 -0600

    Cleanup main plotting file.

--- simulation_reader/_plotting.py
+++ simulation_reader/_plotting.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+from numpy import ndarray
 from matplotlib.pyplot import Figure, Axes
 
 from typing import List, Tuple, TYPE_CHECKING
@@ -8,194 +9,104 @@ if TYPE_CHECKING:
     from . import SimulationReader
 
 
-phi_label = r"$\phi_{m,g}(r)$ [$\frac{n}{cm^{2}~s}$]"
-
-
-def plot_flux_moments(self: "SimulationReader",
-                      moment: int, groups: List[int] = None,
-                      times: List[float] = None) -> None:
-    """Plot a particular energy group's flux moment at a time `t`.
+def plot_power_densities(self: "SimulationReader",
+                         times: List[float] = None) -> None:
+    """Plot power densities at the various times.
 
     Parameters
     ----------
-    moment : int
-        The moment index to plot.
-    groups : List[int]
-        The group indices to plot.
     times : List[float
         The times to plot the flux moment at.
     """
+    times = self._validate_times(times)
 
-    # Get the groups to plot
-    if groups is None:
-        groups = [0]
-    if groups == -1:
-        groups = [g for g in range(self.n_groups)]
-    if isinstance(groups, int):
-        groups = [groups]
+    # Get power densities
+    P = self._interpolate(times, self.power_density)
 
-    # Get the times to plot
-    if times is None:
-        times = [self.times[0], self.times[-1]]
-    if isinstance(times, float):
-        times = [times]
+    # Define labels for plots
+    labels = [f"Time {t:.3f} sec" for t in times]
 
+    # Plot 1D profiles
     if self.dim == 1:
-        self._plot_1d_flux_moments(moment, groups, times)
+        title = "Power Density"
+        ylabel = r"$P(z)$ [$\frac{W}{cm^{3}}$]"
+        self._plot_1d_cell_centered(P, labels, title, ylabel)
+
+    # Plot 2D profiles
     elif self.dim == 2:
-        self._plot_2d_flux_moments(moment, groups, times)
+        x = np.array([p.x for p in self.nodes])
+        y = np.array([p.y for p in self.nodes])
+        X, Y = np.meshgrid(np.unique(x), np.unique(y))
 
-def _plot_1d_flux_moments(self: "SimulationReader",
-                         moment: int, groups: List[int],
-                         times: List[float]) -> None:
-    """Plot 1D flux moments.
+        # Subplot dimentsions
+        n_rows, n_cols = self._format_subplots(len(times))
 
-    Parameters
-    ----------
-    moment : int
-        The moment index to plot.
-    groups : List[int]
-        The group indices to plot.
-    times : List[float
-        The times to plot the flux moment at.
-    """
-    shape = (len(times), self.n_cells)
-
-    # Get the grid
-    z = np.array([p.z for p in self.nodes])
-
-    # Loop over groups
-    for group in groups:
-        fig: Figure = plt.figure()
-        ax: Axes = fig.add_subplot(1, 1, 1)
-        ax.set_title(f"Moment {moment} Group {group} "
-                     f"Flux Moments")
-        ax.set_xlabel("z [cm]")
-        ax.set_ylabel(f"{phi_label}")
-
-        # Get the flux moments at specified times
-        shape = (len(times), self.n_nodes)
-        phi = np.zeros(shape, dtype=float)
-        for t, time in enumerate(times):
-            phi[t] = self.get_group_flux_moment(moment, group, time)
-        phi /= np.max(phi)
-
-        # Plot the flux moments
-        for t, time in enumerate(times):
-            label = f"Time = {time:.3f} sec"
-            ax.plot(z, phi[t], label=label)
-        ax.legend()
-        ax.grid(True)
-        fig.tight_layout()
-
-
-def _plot_2d_flux_moments(self: "SimulationReader",
-                         moment: int, groups: List[int],
-                         times: List[float]) -> None:
-    """Plot 2D flux moments.
-
-    Parameters
-    ----------
-    moment : int
-        The moment index to plot.
-    groups : List[int]
-        The group indices to plot.
-    times : List[float
-        The times to plot the flux moment at.
-    """
-    x = np.array([p.x for p in self.nodes])
-    y = np.array([p.y for p in self.nodes])
-    X, Y = np.meshgrid(np.unique(x), np.unique(y))
-
-    # Subplot dimentsions
-    n_rows, n_cols = _format_subplots(len(times))
-
-    # Loop over groups
-    for group in groups:
+        # Initialize figure
         figsize = (4*n_cols, 4*n_rows)
         fig: Figure = plt.figure(figsize=figsize)
-        fig.suptitle(f"Moment {moment} Group {group} "
-                     f"Flux Moment")
+        fig.suptitle(r"P(x, y) [$\frac{W}{cm^{3}}$]")
 
-        # Get the flux moments at specified times
-        shape = (len(times), self.n_nodes)
-        phi = np.zeros(shape, dtype=float)
         for t, time in enumerate(times):
-            phi[t] = self.get_group_flux_moment(moment, group, time)
-        phi /= np.max(phi)
-
-        # Plot the flux moments
-        for t, time in enumerate(times):
-            phi_fmtd = phi[t].reshape(X.shape)
+            P_fmtd = P[t].reshape(X.shape)
 
             ax: Axes = fig.add_subplot(n_rows, n_cols, t + 1)
             ax.set_xlabel("X [cm]")
             ax.set_ylabel("Y [cm]")
             ax.set_title(f"Time = {t:.3f} sec")
-            im = ax.pcolor(X, Y, phi_fmtd, cmap="jet", shading="auto",
-                           vmin=0.0, vmax=phi.max())
+            im = ax.pcolor(X, Y, P_fmtd, cmap="jet", shading="auto",
+                           vmin=0.0, vmax=P.max())
             fig.colorbar(im)
         fig.tight_layout()
 
 
-def plot_power_density(self: "SimulationReader", time: float) -> None:
-    """Plot the power density at time `t`
+def plot_temperatures(self: "SimulationReader",
+                      times: List[float]) -> None:
+    """Plot temperatures at various times.
 
     Parameters
     ----------
-    time : float
-        The time to plot the power density at.
+    times : List[float
+        The times to plot the temperatures at.
     """
-    power_density = self._interpolate(time, self.power_density)
-    grid = np.array([[p.x, p.y, p.z] for p in self.centroids])
+    times = self._validate_times(times)
 
-    plt.figure()
-    plt.title(f"Power Density\nTime = {time} sec")
-    if np.sum(grid[:, :2]) == 0.0:
-        plt.xlabel("z [cm]")
-        plt.ylabel(r"P(z) $[ \frac{W}{cm^{3}} ]$")
-        plt.plot(grid[:, 2], power_density, "*-b")
-    plt.grid(True)
-    plt.tight_layout()
+    # Get power densities
+    T = self._interpolate(times, self.temperature)
 
+    # Define labels for plots
+    labels = [f"Time {t:.3f} sec" for t in times]
 
-def plot_temperature(self: "SimulationReader", time: float) -> None:
-    """Plot the temperature at time `t`
+    # Plot 1D profiles
+    if self.dim == 1:
+        title = "Temperature"
+        ylabel = "T(z) [K]"
+        self._plot_1d(T, labels, title, ylabel)
 
-    Parameters
-    ----------
-    time : float
-        The time to plot the temperature at.
-    """
-    temperature = self._interpolate(time, self.temperature)
-    grid = np.array([[p.x, p.y, p.z] for p in self.centroids])
+    # Plot 2D profiles
+    elif self.dim == 2:
+        x = np.array([p.x for p in self.nodes])
+        y = np.array([p.y for p in self.nodes])
+        X, Y = np.meshgrid(np.unique(x), np.unique(y))
 
-    plt.figure()
-    plt.title(f"Temperature\nTime = {time} sec")
-    if np.sum(grid[:, :2]) == 0.0:
-        plt.xlabel("z (cm)")
-        plt.ylabel("T(z) [K]")
-        plt.plot(grid[:, 2], temperature, "-*b")
+        # Subplot dimentsions
+        n_rows, n_cols = self._format_subplots(len(times))
 
-    plt.grid(True)
-    plt.tight_layout()
+        # Initialize figure
+        figsize = (4 * n_cols, 4 * n_rows)
+        fig: Figure = plt.figure(figsize=figsize)
+        fig.suptitle("T(x, y) [K]")
 
+        for t, time in enumerate(times):
+            T_fmtd = T[t].reshape(X.shape)
 
-def plot_precursors(self: "SimulationReader",
-                    material_id: int, species: int,
-                    time: float) -> None:
-    precursors = self.get_precursor_j(material_id, species, time)
-    grid = np.array([[p.x, p.y, p.z] for p in self.centroids])
-
-    plt.figure()
-    plt.title(f"Precursor Species {species}, Material {material_id}\n"
-              f"Time = {time} sec")
-    if np.sum(grid[:, :2]) == 0.0:
-        plt.xlabel("z (cm)")
-        plt.ylabel(r"$C_{j}(z)$ $[ \frac{n}{cm^{3}} ]$")
-        plt.plot(grid[:, 2], precursors, "-*b")
-    plt.grid(True)
-    plt.tight_layout()
+            ax: Axes = fig.add_subplot(n_rows, n_cols, t + 1)
+            ax.set_xlabel("X [cm]")
+            ax.set_ylabel("Y [cm]")
+            ax.set_title(f"Time = {t:.3f} sec")
+            im = ax.pcolor(X, Y, T_fmtd, cmap="jet", shading="auto",
+                           vmin=0.0, vmax=T.max())
+            fig.colorbar(im)
+        fig.tight_layout()
 
 
 def plot_power(self: "SimulationReader") -> None:
@@ -240,25 +151,18 @@ def plot_power(self: "SimulationReader") -> None:
     plt.tight_layout()
 
 
-def _format_subplots(n_plots: int) -> Tuple[int, int]:
-    """Determine the number of rows and columns for subplots.
+def _plot_cell_centered_data(self: "SimulationReader",
+                             data: ndarray, labels: List[str] = None,
+                             title: str = None, ylabel: str = None) -> None:
 
-    Parameters
-    ----------
-    n_plots : int
-        The number of subplots that will be used.
 
-    """
-    n_rows, n_cols = 1, 1
-    if n_plots < 4:
-        n_rows, n_cols = 1, 3
-    elif 4 <= n_plots < 9:
-        ref = int(np.ceil(np.sqrt((n_plots))))
-        n_rows = n_cols = ref
-        for n in range(1, n_cols + 1):
-            if n * n_cols >= n_plots:
-                n_rows = n
-                break
-    else:
-        raise AssertionError("Maximum number of plots is 9.")
-    return n_rows, n_cols
+    plt.figure()
+    plt.title("" if title is None else title)
+    plt.xlabel("z [cm]")
+    plt.ylabel("" if ylabel is None else ylabel)
+
+    for t, time in enumerate(times):
+        plt.plot(z, data[t], label=labels[t])
+    plt.grid(True)
+    plt.legend()
+    plt.tight_layout()
