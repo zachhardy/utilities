a1bf568768565ddb19e74f93401e67231adc570b
Zachary Hardy <zhardy1115@gmail.com>
2021-09-27 16:01:45 -0600

    Separated plotting routines for smaller files.

---
+++ simulation_reader/_plot_flux_moments.py
@@ -0,0 +1,129 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from matplotlib.pyplot import Figure, Axes
+
+from typing import List, Tuple, TYPE_CHECKING
+if TYPE_CHECKING:
+    from . import SimulationReader
+
+phi_label = r"$\phi_{m,g}(r)$ [$\frac{n}{cm^{2}~s}$]"
+
+
+def plot_flux_moments(self: "SimulationReader",
+                      moment: int, groups: List[int] = None,
+                      times: List[float] = None) -> None:
+    """Plot groupwise flux moments at various times.
+
+    Parameters
+    ----------
+    moment : int
+        The moment index to plot.
+    groups : List[int]
+        The group indices to plot.
+    times : List[float
+        The times to plot the flux moment at.
+    """
+
+    # Get the groups to plot
+    if groups is None:
+        groups = [0]
+    if groups == -1:
+        groups = [g for g in range(self.n_groups)]
+    if isinstance(groups, int):
+        groups = [groups]
+
+    # Get the times to plot
+    time = self._validate_times(times)
+
+    if self.dim == 1:
+        self._plot_1d_flux_moments(moment, groups, times)
+    elif self.dim == 2:
+        self._plot_2d_flux_moments(moment, groups, times)
+
+def _plot_1d_flux_moments(self: "SimulationReader",
+                         moment: int, groups: List[int],
+                         times: List[float]) -> None:
+    """Plot 1D groupwise flux moments at various times.
+
+    Parameters
+    ----------
+    moment : int
+        The moment index to plot.
+    groups : List[int]
+        The group indices to plot.
+    times : List[float
+        The times to plot the flux moment at.
+    """
+    shape = (len(times), self.n_cells)
+
+    # Get the grid
+    z = np.array([p.z for p in self.nodes])
+
+    # Loop over groups
+    for group in groups:
+        fig: Figure = plt.figure()
+        ax: Axes = fig.add_subplot(1, 1, 1)
+        ax.set_title(f"Moment {moment} Group {group} "
+                     f"Flux Moments")
+        ax.set_xlabel("z [cm]")
+        ax.set_ylabel(f"{phi_label}")
+
+        # Get the flux moments at specified times
+        phi = self.get_group_flux_moment(moment, group, times)
+        phi /= np.max(phi)
+
+        # Plot the flux moments
+        for t, time in enumerate(times):
+            label = f"Time = {time:.3f} sec"
+            ax.plot(z, phi[t], label=label)
+        ax.legend()
+        ax.grid(True)
+        fig.tight_layout()
+
+
+def _plot_2d_flux_moments(self: "SimulationReader",
+                         moment: int, groups: List[int],
+                         times: List[float]) -> None:
+    """Plot 2D groupwise flux moments at various times.
+
+    Parameters
+    ----------
+    moment : int
+        The moment index to plot.
+    groups : List[int]
+        The group indices to plot.
+    times : List[float
+        The times to plot the flux moment at.
+    """
+    X, Y = self.get_nodes()
+
+    # Subplot dimentsions
+    n_rows, n_cols = self._format_subplots(len(times))
+
+    # Loop over groups
+    for group in groups:
+        figsize = (4*n_cols, 4*n_rows)
+        fig: Figure = plt.figure(figsize=figsize)
+        fig.suptitle(f"Moment {moment} Group {group} "
+                     f"Flux Moment")
+
+        # Get the flux moments at specified times
+        shape = (len(times), self.n_nodes)
+        phi = np.zeros(shape, dtype=float)
+        for t, time in enumerate(times):
+            phi[t] = self.get_group_flux_moment(moment, group, time)
+        phi /= np.max(phi)
+
+        # Plot the flux moments
+        for t, time in enumerate(times):
+            phi_fmtd = phi[t].reshape(X.shape)
+
+            ax: Axes = fig.add_subplot(n_rows, n_cols, t + 1)
+            ax.set_xlabel("X [cm]")
+            ax.set_ylabel("Y [cm]")
+            ax.set_title(f"Time = {t:.3f} sec")
+            im = ax.pcolor(X, Y, phi_fmtd, cmap="jet", shading="auto",
+                           vmin=0.0, vmax=phi.max())
+            fig.colorbar(im)
+        fig.tight_layout()
---
+++ simulation_reader/_plot_precursors.py
@@ -0,0 +1,91 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from matplotlib.pyplot import Figure, Axes
+
+from typing import List, Tuple, TYPE_CHECKING
+if TYPE_CHECKING:
+    from . import SimulationReader
+
+
+def plot_precursors(self: "SimulationReader",
+                    species: List[Tuple[int, int]],
+                    times: List[float] = None) -> None:
+    """Plot precursor species at various times.
+
+    Parameters
+    ----------
+    species : List[Tuple[int, int]]
+        The precursor species to plot. The first entry
+        in the descriptor tuple is the material ID and the
+        second is the local precursor ID.
+    times : List[float]
+        The times to plot the precursors at.
+    """
+    # Get the species to plot
+    if species is None:
+        species = [(0, 0)]
+    elif isinstance(species, tuple):
+        species = [species]
+
+    # Get the times to plot
+    times = self._validate_times(times)
+
+    if self.dim == 1:
+        z = np.array([p.z for p in self.centroids])
+
+        # Loop over species
+        for species_ in species:
+            fig: Figure = plt.figure()
+            ax: Axes = fig.add_subplot(1, 1, 1)
+            ax.set_title(f"Material {species_[0]} "
+                         f"Precursor Species {species_[1]}")
+            ax.set_xlabel("z [cm]")
+            ax.set_ylabel(r"$C_{i,j}(z)$ $[$\frac{#}{cm^3}$]")
+
+            # Get the precursors at specified times
+            C = self.get_precursor_species(species_, times)
+            C /= np.max(C)
+
+            # Plot the precursors
+            for t, time in enumerate(times):
+                label = f"Time = {time:.3f} sec"
+                ax.plot(z, C[t], label=label)
+            ax.legend()
+            ax.grid(True)
+            fig.tight_layout()
+
+    elif self.dim == 2:
+        x = np.array([p.x for p in self.nodes])
+        y = np.array([p.y for p in self.nodes])
+        X, Y = np.meshgrid(np.unique(x), np.unique(y))
+
+        # Subplot dimentsions
+        n_rows, n_cols =self._format_subplots(len(times))
+
+        # Loop over groups
+        for species_ in species:
+            figsize = (4 * n_cols, 4 * n_rows)
+            fig: Figure = plt.figure(figsize=figsize)
+            fig.suptitle(f"Material {species_[0]} "
+                         f"Precursor Species {species_[1]}")
+
+            # Get the flux moments at specified times
+            shape = (len(times), self.n_cells)
+            C = np.zeros(shape, dtype=float)
+            for t, time in enumerate(times):
+                C[t] = self.get_precursor_species(species_, time)
+            C /= np.max(C)
+
+            # Plot the flux moments
+            for t, time in enumerate(times):
+                C_fmtd = C[t].reshape(X.shape)
+
+                ax: Axes = fig.add_subplot(n_rows, n_cols, t + 1)
+                ax.set_xlabel("X [cm]")
+                ax.set_ylabel("Y [cm]")
+                ax.set_title(f"Time = {t:.3f} sec")
+                im = ax.pcolor(X, Y, C_fmtd, cmap="jet", shading="auto",
+                               vmin=0.0, vmax=C.max())
+                fig.colorbar(im)
+            fig.tight_layout()
